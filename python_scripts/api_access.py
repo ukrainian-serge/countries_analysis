50f966f6c8ed7a1ca59cabe266c100487e7c6916
ukrainian-serge <sergekamilchu@gmail.com>
2019-12-06 11:51:26 -0600

    working on pulling data

---
+++ python_scripts/api_access.py
@@ -0,0 +1,19 @@
+
+
+# import pandas as pd
+import os
+
+# ocean = pd.read_csv('../data_gdp_and_others/ocean_electricity_production_total.csv')
+# solar = pd.read_csv('..\\data_gdp_and_others/solar_electricity_production_total.csv')
+# thermal = pd.read_csv('..\\data_gdp_and_others/thermal_production_total.csv')
+
+# print(ocean.shape)
+# print(solar.shape)
+# print(thermal.shape)
+
+os.
+
+
+
+# msg = "Hello World"
+# print(msg)
